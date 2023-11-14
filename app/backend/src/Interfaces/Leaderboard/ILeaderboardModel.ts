e5a32d4688c84a68b8ccf8fd58bf16494c195557
João <costaaraujojoaopedro@gmail.com>
2023-11-14 10:49:26 -0300

    FEAT: implementa classificações filtradas

--- app/backend/src/Interfaces/Leaderboard/ILeaderboardModel.ts
+++
@@ -1,5 +0,0 @@
-import ILeaderboard from './ILeaderboard';
-
-export default interface ILeaderboardModel {
-  getAllLeaderboard(): Promise<ILeaderboard[]>;
-}
--- app/backend/src/services/LeaderBord.service.ts
+++ app/backend/src/services/LeaderBord.service.ts
@@ -1,47 +1,60 @@
 import { Request, Response } from 'express';
 import ITeam from '../Interfaces/Teams/Team';
 import IMatch from '../Interfaces/Match/IMatch';
-// import ILeaderboard from '../Interfaces/Leaderboard/ILeaderboard';
 import Match from '../database/models/MatchModels';
 import Team from '../database/models/TeamsModel';
 
 export default class LeaderboardService {
-  static async getHomeLeaderboard(_req: Request, _res: Response) {
+  static async getHomeLeaderboard(req: Request, res: Response) {
     const teams = await Team.findAll();
     const matches = await Match.findAll({ where: { inProgress: false } });
+  
+    const leaderboard = teams.map((team) => LeaderboardService.calculateStats(team, matches));
+  
+    leaderboard.sort((a, b) => {
+      if (a.totalPoints !== b.totalPoints) {
+        return b.totalPoints - a.totalPoints;
+      }
+      if (a.totalVictories !== b.totalVictories) {
+        return b.totalVictories - a.totalVictories;
+      }
+      if ((a.goalsFavor - a.goalsOwn) !== (b.goalsFavor - b.goalsOwn)) {
+        return (b.goalsFavor - b.goalsOwn) - (a.goalsFavor - a.goalsOwn);
+      }
+      return b.goalsFavor - a.goalsFavor;
+    });
+  
+    return leaderboard;
+  }
 
-    return teams.map((team) => LeaderboardService.calculateStats(team, matches));
+  static calculateTeamStats(homeMatches: IMatch[]) {
+    const totalGames = homeMatches.length;
+    const totalVictories = LeaderboardService.calculateVictories(homeMatches);
+    const totalDraws = LeaderboardService.calculateDraws(homeMatches);
+    const goalsFavor = LeaderboardService.calculateGoalsFavor(homeMatches);
+    const goalsOwn = LeaderboardService.calculateGoalsOwn(homeMatches);
+
+    return {
+      totalPoints: totalVictories * 3 + totalDraws,
+      totalGames,
+      totalVictories,
+      totalDraws,
+      totalLosses: totalGames - totalVictories - totalDraws,
+      goalsFavor,
+      goalsOwn,
+      goalsBalance: goalsFavor - goalsOwn,
+      efficiency: Math
+        .round(((totalVictories * 3 + totalDraws) / (totalGames * 3)) * 100 * 100) / 100,
+    };
   }
 
   static calculateStats(team: Partial<ITeam>, matches: IMatch[]) {
     const homeMatches = matches.filter((match) => match.homeTeamId === team.id);
-    const totalGames = homeMatches.length;
-    const totalVictories = LeaderboardService.calculateVictories(homeMatches);
-    const totalDraws = LeaderboardService.calculateDraws(homeMatches);
-    const totalLosses = totalGames - totalVictories - totalDraws;
-    const goalsFavor = LeaderboardService.calculateGoalsFavor(homeMatches);
-    const goalsOwn = LeaderboardService.calculateGoalsOwn(homeMatches);
-    const totalPoints = totalVictories * 3 + totalDraws;
-    // console.log(
-    //   team.teamName,
-    //   homeMatches,
-    //   totalPoints,
-    //   totalGames,
-    //   totalVictories,
-    //   totalDraws,
-    //   totalLosses,
-    //   goalsFavor,
-    //   goalsOwn,
-    // );
+    const teamStats = LeaderboardService.calculateTeamStats(homeMatches);
+
     return {
       name: team.teamName,
-      totalPoints,
-      totalGames,
-      totalVictories,
-      totalDraws,
-      totalLosses,
-      goalsFavor,
-      goalsOwn,
+      ...teamStats,
     };
   }
 
